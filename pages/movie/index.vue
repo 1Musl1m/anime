35cc0bd2911694267171d14de04a041819948088
Саша <115547303+1Musl1m@users.noreply.github.com>
2024-05-06 20:47:40 +0300

    button using addData

--- pages/movie/index.vue
+++ pages/movie/index.vue
@@ -1,38 +1,33 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { LoadingOutlined } from "@ant-design/icons-vue";
-import { h } from "vue";
-import type { Anime } from "~/types/anime";
-
-const indicator = h(LoadingOutlined, {
-    style: {
-        fontSize: "24px",
-    },
-    spin: true,
-});
+import type { Anime } from '~/types/anime';
 
 const page = ref(1);
-const { pending, data: animes } = useFetch<Anime[]>(
-    `https://shikimori.one/api/animes?page=${page.value}&limit=49&order=popularity`,
-    {
-        lazy: true,
-    }
+const { data: animes } = useFetch<Anime[]>(() =>
+  `https://shikimori.one/api/animes?page=${page.value}&limit=49&order=popularity`
 );
+
+
+const addData = () => {
+  page.value++;
+};
 </script>
 
 <template>
-    <div class="container flex flex-col">
-        <h1 class="text-3xl font-bold text-center">Список аниме и фильмов</h1>
-        <div class="flex items-center justify-between flex-wrap mt-10 px-4">
-            <a-spin :indicator="indicator" v-if="pending" />
-            <AnimeCard
-                v-for="anime in animes"
-                :key="anime.id"
-                :anime="anime"
-                v-else
-            />
-        </div>
-        <a-back-top />
+  <div class="container flex flex-col">
+    <h1 class="text-3xl font-bold text-center">Список аниме и фильмов</h1>
+    <div class="flex items-center justify-between flex-wrap mt-10 px-4">
+      <AnimeCard
+        v-for="anime in animes"
+        :key="anime.id"
+        :anime="anime"
+      />
     </div>
-    <!-- <a-button>Добавить</a-button> -->
+    <button
+      class="mx-auto mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+      @click="addData"
+    >
+      Еще
+    </button>
+    <a-back-top />
+  </div>
 </template>
