052a36028db18a4f1e6a7cf2a71f99f1302dabca
Саша <115547303+1Musl1m@users.noreply.github.com>
2024-05-06 22:05:07 +0300

    complete addData

--- pages/movie/index.vue
+++ pages/movie/index.vue
@@ -1,33 +1,43 @@
 <script setup lang="ts">
-import type { Anime } from '~/types/anime';
+import { ref, onMounted } from 'vue';
+import type { Anime } from "~/types/anime";
 
 const page = ref(1);
-const { data: animes } = useFetch<Anime[]>(() =>
-  `https://shikimori.one/api/animes?page=${page.value}&limit=49&order=popularity`
-);
+const animes = ref<Anime[]>([]);
 
+const fetchAnimes = async () => {
+    try {
+        const response = await fetch(`https://shikimori.one/api/animes?page=${page.value}&limit=28&order=popularity`);
+        if (!response.ok) {
+            throw new Error('Проблема с запросом к API');
+        }
+        const newAnimes = await response.json();
+        animes.value = [...animes.value, ...newAnimes];
+    } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+    }
+};
 
 const addData = () => {
-  page.value++;
+    page.value++;
+    fetchAnimes();
 };
+
+onMounted(fetchAnimes);
 </script>
 
 <template>
-  <div class="container flex flex-col">
-    <h1 class="text-3xl font-bold text-center">Список аниме и фильмов</h1>
-    <div class="flex items-center justify-between flex-wrap mt-10 px-4">
-      <AnimeCard
-        v-for="anime in animes"
-        :key="anime.id"
-        :anime="anime"
-      />
+    <div class="container flex flex-col">
+        <h1 class="text-3xl font-bold text-center">Список аниме и фильмов</h1>
+        <div class="flex items-center gap-3 flex-wrap mt-10 px-4">
+            <AnimeCard v-for="anime in animes" :key="anime.id" :anime="anime" />
+        </div>
+        <button
+            class="mx-auto mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            @click="addData"
+        >
+            Еще
+        </button>
+        <a-back-top />
     </div>
-    <button
-      class="mx-auto mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-      @click="addData"
-    >
-      Еще
-    </button>
-    <a-back-top />
-  </div>
 </template>
